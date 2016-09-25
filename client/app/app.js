ad828f7d5de63252b9c7065f5ed2dd782ebd426a
Val Saven <val.saven@gmail.com>
2016-09-25 22:53:16 +0300

    Add main page

--- client/app/app.js
+++ client/app/app.js
@@ -3,3 +3,20 @@
  */
 
 'use strict';
+
+import angular from 'angular';
+import './app.css';
+
+angular
+  .module('myBookListApp', [main])
+  .config(['$urlRouterProvider, $locationProvider',
+    function ($urlRouterProvider, $locationProvider) {
+      'ngInject';
+      $urlRouterProvider.otherwise('/');
+      $locationProvider.html5Mode(true);
+    }]);
+
+angular.element(document)
+  .ready(() => {
+    
+  });

72272bb9d762386a75d8b040a37f6f6d6c96f2a0
Val Saven <val.saven@gmail.com>
2016-10-18 18:13:10 +0300

    feat(header): Move to a separate component

--- client/app/app.js
+++ client/app/app.js
@@ -9,10 +9,11 @@ import uiRouter from 'angular-ui-router';
 
 import { routeConfig } from './app.config';
 import main from './main/main.component';
+import header from './components/header/header.component';
 import sidemenu from './components/sidemenu/sidemenu.component';
 import './app.css';
 
-angular.module('myBookListApp', [uiRouter, main, sidemenu])
+angular.module('myBookListApp', [uiRouter, main, header, sidemenu])
   .config(routeConfig);
 
 angular.element(document)
--- client/app/components/header/header.component.js
+++ client/app/components/header/header.component.js
@@ -1,3 +1,14 @@
 /**
  * Created by val on 10/17/16.
  */
+
+import angular from 'angular';
+
+export class HeaderComponent {}
+
+export default angular.module('HeaderComponent', [])
+  .component('header', {
+    template: require('./header.html'),
+    controller: HeaderComponent
+  })
+  .name;
